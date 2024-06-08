9981e81cbf20bd9f126d77ee8fbed89a3cf6c14c
Kimon Meier <kimon.meier@gmail.com>
2024-06-08 14:59:53 +0200

    Changed the volume of the buzzer sound. Buzzer can now be pressed via space

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -7,8 +7,9 @@
 	import { buzzerSound } from '$lib/store/AudioStore';
 	import { Sound } from 'svelte-sound';
 	import buzzerSoundFile from "$lib/assets/buzzer.mp3";
+	import { ClientEvents } from 'gameshow-lib/enums/ClientEvents';
 
-	$buzzerSound = new Sound(buzzerSoundFile);
+	$buzzerSound = new Sound(buzzerSoundFile, {volume: 0.4});
 
 	onMount(() => {
 		App.getInstance().startApp();
@@ -18,10 +19,19 @@
 		App.getInstance().stopApp();
 	}
 
+	function keyDown(e: KeyboardEvent) {
+		if (e.keyCode == 32) {
+			App.getInstance().sendMessage({
+				type: ClientEvents.PLAYER_BUZZER_PRESSED,
+				time: Date.now(),
+			});
+		}
+	}
+
 </script>
 
 
-<svelte:window on:beforeunload={beforeunload}/>
+<svelte:window on:beforeunload={beforeunload} on:keydown={keyDown}/>
 <div class="bg-slate-900 text-white w-full h-full flex flex-col">
 	{#if $alertStore}
 		<Alert message={$alertStore.message} alertType={$alertStore.alertType} />
