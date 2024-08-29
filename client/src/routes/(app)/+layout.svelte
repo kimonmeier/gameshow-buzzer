752a8099c527cb1df57bc69aced24b553601f2c1
Kimon Meier <kimon.meier@gmail.com>
2024-08-29 22:58:24 +0200

    Fehler behoben bei welchem nicht überprüft wurde ob der Buzzer gesperrt war oder nicht und ein Spieler immer buzzern konnte

--- client/src/routes/(app)/+layout.svelte
+++ client/src/routes/(app)/+layout.svelte
@@ -6,19 +6,19 @@
 	import Alert from '$lib/components/alert/Alert.svelte';
 	import { answerRightSound, answerWrongSound, buzzerSound } from '$lib/store/AudioStore';
 	import { Sound } from 'svelte-sound';
-	import buzzerSoundFile from "$lib/assets/buzzer.mp3";
-	import answerRightSoundFile from "$lib/assets/right_answer.wav";
-	import answerWrongSoundFile from "$lib/assets/wrong_answer.wav";
+	import buzzerSoundFile from '$lib/assets/buzzer.mp3';
+	import answerRightSoundFile from '$lib/assets/right_answer.wav';
+	import answerWrongSoundFile from '$lib/assets/wrong_answer.wav';
 	import { ClientEvents } from 'gameshow-lib/enums/ClientEvents';
-	import { isInputFocused } from '$lib/store/InputStore';
+	import { isBuzzerLocked } from '$lib/store/BuzzerStore';
 
-	$buzzerSound = new Sound(buzzerSoundFile, {volume: 0.4});
-	$answerRightSound = new Sound(answerRightSoundFile, {volume: 0.6});
-	$answerWrongSound = new Sound(answerWrongSoundFile, {volume: 0.5});
+	$buzzerSound = new Sound(buzzerSoundFile, { volume: 0.4 });
+	$answerRightSound = new Sound(answerRightSoundFile, { volume: 0.6 });
+	$answerWrongSound = new Sound(answerWrongSoundFile, { volume: 0.5 });
 
 	onMount(() => {
 		App.getInstance().startApp();
-	})
+	});
 
 	function beforeunload(args: BeforeUnloadEvent) {
 		App.getInstance().stopApp();
@@ -26,19 +26,17 @@
 
 	function keyDown(e: KeyboardEvent) {
 		if (e.keyCode == 32) {
-			if ($isInputFocused) {
+			if ($isBuzzerLocked) {
 				return;
 			}
 			App.getInstance().sendMessage({
-				type: ClientEvents.PLAYER_BUZZER_PRESSED,
+				type: ClientEvents.PLAYER_BUZZER_PRESSED
 			});
 		}
 	}
-
 </script>
 
-
-<svelte:window on:beforeunload={beforeunload} on:keydown={keyDown}/>
+<svelte:window on:beforeunload={beforeunload} on:keydown={keyDown} />
 <div class="bg-slate-900 text-white w-full h-full flex flex-col">
 	{#if $alertStore}
 		<Alert message={$alertStore.message} alertType={$alertStore.alertType} />
