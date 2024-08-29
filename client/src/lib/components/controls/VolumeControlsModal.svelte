34b57d1345cfc6982234ec6c61f560da32d8a1a7
Kimon Meier <kimon.meier@gmail.com>
2024-08-29 23:21:02 +0200

    Einstellungen für die Lautstärke hinzugefügt

---
+++ client/src/lib/components/controls/VolumeControlsModal.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import {
+		answerRightSoundVolume,
+		answerWrongSoundVolume,
+		buzzerSoundVolume
+	} from '$lib/store/AudioStore';
+	import GroupBox from '../groupBox/GroupBox.svelte';
+	import Modal from '../modal/Modal.svelte';
+
+	export let showModal: boolean;
+</script>
+
+<Modal bind:showModal>
+	<GroupBox title="Volume Einstellungen">
+		<div class="flex flex-col w-full gap-5">
+			<div class="flex flex-row">
+				<div>Lautstärke Buzzer:</div>
+
+				<input type="range" bind:value={$buzzerSoundVolume} min={0} max={1} step="0.005" />
+			</div>
+			<div class="flex flex-row">
+				<div>Lautstärke richtige Antwort:</div>
+
+				<input type="range" bind:value={$answerRightSoundVolume} min={0} max={1} step="0.005" />
+			</div>
+			<div class="flex flex-row">
+				<div>Lautstärke falsche Antwort:</div>
+
+				<input type="range" bind:value={$answerWrongSoundVolume} min={0} max={1} step="0.005" />
+			</div>
+		</div>
+	</GroupBox>
+</Modal>
--- client/src/routes/(public)/+layout.svelte
+++ client/src/routes/(public)/+layout.svelte
@@ -3,26 +3,25 @@
 	import '../../app.css';
 	import App from '$lib/services/GameManager';
 	import { answerRightSound, answerWrongSound, buzzerSound } from '$lib/store/AudioStore';
-	import { Sound } from 'svelte-sound';
-	import buzzerSoundFile from "$lib/assets/buzzer.mp3";
-	import answerRightSoundFile from "$lib/assets/right_answer.wav";
-	import answerWrongSoundFile from "$lib/assets/wrong_answer.wav";
+	import buzzerSoundFile from '$lib/assets/buzzer.mp3';
+	import answerRightSoundFile from '$lib/assets/right_answer.wav';
+	import answerWrongSoundFile from '$lib/assets/wrong_answer.wav';
+	import { Sound } from '$lib/components/sound/Sound';
 
-	$buzzerSound = new Sound(buzzerSoundFile, {volume: 0});
-	$answerRightSound = new Sound(answerRightSoundFile, {volume: 0});
-	$answerWrongSound = new Sound(answerWrongSoundFile, {volume: 0});
+	$buzzerSound = new Sound(buzzerSoundFile, undefined, { volume: 0 });
+	$answerRightSound = new Sound(answerRightSoundFile, undefined, { volume: 0 });
+	$answerWrongSound = new Sound(answerWrongSoundFile, undefined, { volume: 0 });
 
 	onMount(() => {
 		App.getInstance().startApp();
-	})
+	});
 
 	function beforeunload(args: BeforeUnloadEvent) {
 		App.getInstance().stopApp();
 	}
-
 </script>
 
-<svelte:window on:beforeunload={beforeunload}/>
+<svelte:window on:beforeunload={beforeunload} />
 <div class="text-white w-full h-full flex flex-col">
 	<slot />
 </div>
